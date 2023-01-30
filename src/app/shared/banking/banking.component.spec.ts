3a63ea7a3c7aa806d003ea6a5c8d8085f2e51791
Geovana Augusta <geovanaag@hotmail.com>
2023-01-30 18:08:58 -0300

    test: inicia os testes unitários

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -20,4 +20,28 @@ describe('BankingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('(U) getPoupanca(): poupanca should have 10', () => {
+    expect(component.getPoupanca).toEqual(10);
+  })
+
+  it('(U) getCarteira(): carteira should have 50', () => {
+    expect(component.getPoupanca).toEqual(50);
+  })
+
+  it('(U) setSacar(): should transfer the value of poupanca from carteira', () => {
+    component.setSacar('10');
+    fixture.detectChanges();
+
+    expect(component.getPoupanca).toEqual(0);
+    expect(component.getCarteira).toEqual(60);
+  })
+
+  it('(U) setDepositar(): should transfer the value of carteira from poupanca', () => {
+    component.setDepositar('10');
+    fixture.detectChanges();
+
+    expect(component.getPoupanca).toEqual(20);
+    expect(component.getCarteira).toEqual(40);
+  })
 });
