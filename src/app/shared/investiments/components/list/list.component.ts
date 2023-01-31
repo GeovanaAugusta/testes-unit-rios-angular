65b5e3c821ba96e91cc98b71f990ecba5b9fc8a5
Geovana Augusta <geovanaag@hotmail.com>
2023-01-31 15:08:51 -0300

    fix: melhora organização dos componentes x pastas

--- src/app/shared/investiments/components/list/list.component.ts
+++ src/app/shared/investiments/components/list/list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { InvestimentsI } from '../model/investiments';
+import { InvestimentsI } from '../../model/investiments';
 
 @Component({
   selector: 'app-list',
